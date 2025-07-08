d7ad0d0652214cb4cd6c1b1f0a3bb0f9f3042ca5
anik-m <fosscode.ozbc8@slmails.com>
2025-07-08 17:15:53 +0600

    changes added

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# --- Update System and Install Base Packages ---
+echo ":: Synchronizing package databases and updating system..."
+sudo pacman -Syu --noconfirm
+
+echo ":: Installing git and base-devel for package building..."
+sudo pacman -S --needed git base-devel --noconfirm
+
+# --- Clone Dotfiles ---
+# Make sure your dotfiles repository is correct
+# echo ":: Cloning dotfiles from repository..."
+# git clone https://github.com/your-username/your-dotfiles-repo.git ~/.dotfiles
+
+# --- Install Stow and Manage Dotfiles ---
+echo ":: Installing GNU Stow to manage dotfiles..."
+sudo pacman -S stow --noconfirm
+
+echo ":: Applying dotfiles with Stow..."
+cd ~/dotfiles
+stow .
+cd  # Return to home directory
+
+# --- Install Paru (AUR Helper) ---
+echo ":: Installing Paru AUR Helper..."
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si --noconfirm
+cd ..
+rm -rf paru # Clean up the cloned directory
+
+# --- Install Packages ---
+echo ":: Installing packages from pkglist.txt using Paru..."
+cd ~/dotfiles
+paru -S --noconfirm - < pkglist.txt
+
+echo "Installation complete! Reboot for all changes to take effect."
